59fa395d5850474f242b83eae3b424e5d16c91a8
Abhishektodupunuri3 <abhishektodupunuri1698@gmail.com>
2024-01-19 10:35:46 -0500

    Auto Update

---
+++ main.tf
@@ -0,0 +1,13 @@
+module "docdb" {
+  source                      = "./vendor/modules/docdb"
+  ENV                         = var.ENV
+  
+
+
+}
+
+
+# You cannot variablize this piece of code in the source parameter
+
+
+
